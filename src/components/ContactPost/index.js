e241c2087e1b6564964a1ff28f713369c4538f69
adi <adiletkub@gmail.com>
2019-08-20 20:13:22 +1000

    finished DetailContacs

--- src/components/ContactPost/index.js
+++ src/components/ContactPost/index.js
@@ -4,13 +4,27 @@ import React from 'react';
 
 import './styles.scss';
 
-const ContactPost = () => {
+const ContactPost = (props) => {
+  const { contacts } = props
 
   return (
-    <div>
-      
-    </div>
-  );
+    <>
+      {contacts[0].posts.map((post, index)=>
+        <div key={post.sentence+index} className='contact-post'>
+          <p className='contact-post__text'>{post.sentence}</p>
+          <p className='contact-post__text'>{post.sentences}</p>
+          <p className='contact-post__text'>{post.paragraph}</p>
+          <div className='contact-post-tags'>
+            {post.words.map((tag, index)=>
+              <div key={tag+index} className='contact-post-tags__tag'>
+                  {tag}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default ContactPost;
